4f07137a91ff8a816873c1f0f1b9b0748742cd22
BuffbelliedHummingbird <frauke.bunsen@awi.de>
2023-09-06 20:29:04 +0200

    interpolate velocity update requires flexible input to compute_vel_elements

--- src/pdaf/compute_vel_elems.F90
+++ src/pdaf/compute_vel_elems.F90
@@ -1,13 +1,13 @@
-subroutine compute_vel_elems()
+subroutine compute_vel_elems(Unode,UV)
 
-    ! USE MOD_MESH
-    ! USE o_PARAM
-    USE o_ARRAYS, &
-       ONLY: UV, Unode
+    ! USE o_ARRAYS, &
+    !  ONLY: UV, Unode
     USE g_PARSUP, &
-       ONLY: myDim_elem2D
+       ONLY: myDim_elem2D, eDim_elem2D, &
+             myDim_nod2D, eDim_nod2D
     USE mod_assim_pdaf, &
        ONLY: mesh_fesom
+    USE g_comm_auto                    ! contains: interface exchange_nod()
 
     IMPLICIT NONE
 
@@ -15,6 +15,10 @@ subroutine compute_vel_elems()
     integer          :: elem, nz, &    ! counters
                         ule, &         ! upper vertical boundary index at element, default==1
                         nle            ! number of levels as element considering bottom topography
+                        
+    ! *** arguments ***
+    real, intent(inout) :: Unode(2,mesh_fesom%nl-1, myDim_nod2D + eDim_nod2D) ! u and v velocities defined at nodes
+    real, intent(out)   :: UV(2,mesh_fesom%nl-1,myDim_elem2D + eDim_elem2D)   ! u and v velocities defined at element centroids
     
     ! *** variables imported from modules ***
     ! ulevels(:)               ! upper boundary index of all vertical element loops, default==1
@@ -31,6 +35,8 @@ subroutine compute_vel_elems()
     ! 2. The medians of a triangle divide the triangle into 6 small triangles with equal area.
     ! Thus, each of the 3 vertices is associated with an equal area share of the element
     ! and weighted equally for interpolation.
+    
+    call exchange_nod(Unode)
     
     elements: DO elem = 1, myDim_elem2D
     
